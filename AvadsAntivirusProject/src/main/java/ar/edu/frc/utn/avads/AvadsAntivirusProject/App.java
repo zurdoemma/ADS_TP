b77f689808eccadaf3afaff25b46fd3eac8c76b2
ecarballo <ecarballo@admcal02.libertadsa.local>
2017-04-20 16:01:04 -0300

    Initial Version

---
+++ AvadsAntivirusProject/src/main/java/ar/edu/frc/utn/avads/AvadsAntivirusProject/App.java
@@ -0,0 +1,13 @@
+package ar.edu.frc.utn.avads.AvadsAntivirusProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
