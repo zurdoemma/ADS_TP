e56bf86510a97aacc7a86fc18f72f232a0508b13
ECarballo <zurdoemma@hotmail.com>
2017-05-31 17:00:14 -0300

    Se modifica el archivo de propiedades y se agrega el servicio para la base de datos.

--- AvadsCommonProject/src/main/java/ar/edu/frc/utn/avads/util/PropertiesUtil.java
+++ AvadsCommonProject/src/main/java/ar/edu/frc/utn/avads/util/PropertiesUtil.java
@@ -1,7 +1,10 @@
 package ar.edu.frc.utn.avads.util;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -39,9 +42,35 @@ public class PropertiesUtil {
 			value = properties.getProperty(key);
 		} else {
 			cargarPropiedades();
+                        value = properties.getProperty(key);
 		}
 
 		return value;
 	}
+        
+	public static String setProperty(String key, String value) {
+
+		if (properties != null) {
+			properties.setProperty(key, value);
+		} else {
+			cargarPropiedades();
+                        properties.setProperty(key, value);
+		}
+                
+                OutputStream output = null;
+                try {
+                    output = new FileOutputStream("/avads.properties");
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(PropertiesUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }        
+                try {
+                    properties.store(output, null);
+                    output.close();        
+                } catch (IOException ex) {
+                    Logger.getLogger(PropertiesUtil.class.getName()).log(Level.SEVERE, null, ex);
+                }                
+
+		return value;
+	}        
 
 }
